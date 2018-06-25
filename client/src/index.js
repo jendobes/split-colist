fd561dd924a439ff97c05efa6a59727960f31bf5
jendobes <jdobak1@gmail.com>
2018-06-25 15:27:28 -0400

    add thunk and dependencies to index js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
+
+ReactDOM.render(
+  <Provider>
+  <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
