4acba523f3469b060b16f7a561bdb7baaadfb537
jendobes <jdobak1@gmail.com>
2018-06-26 18:43:11 -0400

    api controllers render json

--- app/controllers/colivings_controller.rb
+++ app/controllers/colivings_controller.rb
@@ -1,2 +1,13 @@
 class ColivingsController < ApplicationController
+
+  def index
+    @colivings = Coliving.all
+    render json: @colivings
+  end
+
+  def show
+    @coliving = Coliving.find(params: id)
+    render json: @coliving
+  end
+  
 end
--- app/controllers/coworkings_controller.rb
+++ app/controllers/coworkings_controller.rb
@@ -1,11 +1,13 @@
 class CoworkingsController < ApplicationController
 
   def index
-    coworkings = Coworking.all
+    @coworkings = Coworking.all
+    render json: @coworkings
   end
 
   def show
-    coworking = Coworking.find(params: id)
+    @coworking = Coworking.find(params: id)
+    render json: @coworking
   end
 
 end
