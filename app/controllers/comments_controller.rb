68f9b67a457f6089b21964aac7e2b2c26b0dfee7
jendobes <jdobak1@gmail.com>
2018-06-26 19:50:25 -0400

    add before filter to comments index to check if comments is nested resource

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,20 @@
 class CommentsController < ApplicationController
+  before_filter :get_parent
+
+  def get_parent
+    if params[:coliving_id]
+      @parent = Coliving.find(params[:coliving_id])
+    else params[:coworking_id]
+      @parent = Coworking.find(params[:coworking_id])
+    end
+  end
 
   def index
-    @comments = Comment.all
+    @parent ? @comments = @parent.comments : @comments = Comment.all
     render json: @comments
   end
 
-  
+
+
+
 end
