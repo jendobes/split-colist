4468732822cb1a1ee94d0b80d94fbb8ccbbceb1c
jendobes <jdobak1@gmail.com>
2018-06-28 17:30:27 -0400

    colist header is now clickable link that renders home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
     <div className="App">
       <div className="App-header">
-        <h1>co-list</h1>
+        <h1><Link to='/' style={{ textDecoration: 'none', color: 'white'}}>co-list</Link></h1>
         <p> find nomad approved co-living and co-working spaces around the world </p>
       </div>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import Home from './components/Home';
 import Header from './components/Header';
-import Cospace from './containers/Coworking'
+import Cospace from './containers/Cospace'
 import registerServiceWorker from './registerServiceWorker';
 
 // const store = createStore(
