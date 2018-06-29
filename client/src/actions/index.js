8f4b9df1f8714b49dbecc2947a015003ef31bc63
jendobes <jdobak1@gmail.com>
2018-06-29 17:51:26 -0400

    split indexes fetch requests into loading and get requests

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,17 @@
 export function getCoworkings() {
   return(dispatch) => {
-    dispatch({type: 'GET_COWORKINGS'});
-    fetch('http://localhost:3001/api/coworking')
+    dispatch({type: 'LOAD_COWORKINGS'});
+    fetch('http://localhost:3001/api/coworking').then(response =>{
+      return response.json()
+    }).then(coworkings => dispatch({type: 'GET_COWORKINGS', payload: coworkings}))
   }
 }
 
 export function getColivings() {
   return(dispatch) => {
-    dispatch({type: 'GET_COLIVINGS'});
-    fetch('http://localhost:3001/api/coliving')
+    dispatch({type: 'LOAD_COLIVINGS'});
+    fetch('http://localhost:3001/api/coliving').then(response =>{
+      return response.json()
+    }).then(colivings => dispatch({type: 'GET_COLIVINGS', payload: colivings}))
   }
 }
