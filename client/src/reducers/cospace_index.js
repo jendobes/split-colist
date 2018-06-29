32369832ec53dc753417e52c289deab3278f7ff2
jendobes <jdobak1@gmail.com>
2018-06-29 17:43:22 -0400

    split coworking and coliving get requests into two reducer actions

--- client/src/reducers/cospace_index.js
+++ client/src/reducers/cospace_index.js
@@ -1,7 +1,10 @@
 export default (state = {}, action) => {
   switch(action.type){
-    case 'GET_COSPACES':
+    case 'GET_COLIVINGS':
       return Object.assign();
+
+    case 'GET_COWORKINGS':
+    return Obeject.assign();
 
   };
 }
