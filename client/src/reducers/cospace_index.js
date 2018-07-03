19e29ed76b73821babdb889868211912c295e943
jendobes <jdobak1@gmail.com>
2018-07-03 10:39:21 -0400

    fix object assign syntax in reducer

--- client/src/reducers/cospace_index.js
+++ client/src/reducers/cospace_index.js
@@ -1,10 +1,10 @@
 export default function cospaceReducer(state = {colivings: [], coworkings: []}, action) {
   switch(action.type){
     case 'GET_COLIVINGS':
-      return Object.assign({}, state, colivings: action.payload);
+      return Object.assign({}, state, {colivings: action.payload});
 
     case 'GET_COWORKINGS':
-    return Object.assign({}, state, coworkings: action.payload);
+    return Object.assign({}, state, {coworkings: action.payload});
 
     default: return state
   };
