cba8b12e3426a67f7f6d6327e1e82459d64c9649
jendobes <jdobak1@gmail.com>
2018-07-03 00:07:44 -0400

    define state in reducer and clarify how actions will change the state

--- client/src/reducers/cospace_index.js
+++ client/src/reducers/cospace_index.js
@@ -1,10 +1,10 @@
-export default function cospaceReducer(state = {}, action) {
+export default function cospaceReducer(state = {colivings: [], coworkings: []}, action) {
   switch(action.type){
     case 'GET_COLIVINGS':
-      return Object.assign({}, state);
+      return Object.assign({}, state, colivings: action.payload);
 
     case 'GET_COWORKINGS':
-    return Object.assign({}, state);
+    return Object.assign({}, state, coworkings: action.payload);
 
     default: return state
   };
