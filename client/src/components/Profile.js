3fdd381ec545c14111e09e82c989da2da0987584
jendobes <jdobak1@gmail.com>
2018-07-06 12:04:00 -0400

    add link to cospace show page in profile card, links to nested show route

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../css/profile.css'
 
 const Profile = (props) => {
 
-  const {name, location, rating, about, website} = props.cospace
+  const {name, location, rating, about, website, id} = props.cospace
 
   return (
     <div className="container">
@@ -13,6 +14,7 @@ const Profile = (props) => {
         <p>About: {about}</p>
         <p>Webiste: {website}</p>
         <p>Rating: {rating}</p>
+        <Link to={`/coliving/${id}`}>View Page</Link>
       </div>
     </div>
   )
