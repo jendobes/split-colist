5e55c2c61e68f9f1c081430ebc1b4268849abe70
jendobes <jdobak1@gmail.com>
2018-07-24 13:16:49 -0400

    home page elements collapse gracefully

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,14 +8,15 @@ class Home extends React.Component {
   render() {
     return(
       <div className="wrapper">
-        <div className="Coliving-container">
-          <p className="text-box"><Link to='/colivings' style={{ textDecoration: 'none', color: 'white'}}>live</Link></p>
-        </div>
+        <div className="wrapper-two">
+          <div className="Coliving-container">
+            <p className="text-box"><Link to='/colivings' style={{ textDecoration: 'none', color: 'white'}}>live</Link></p>
+          </div>
 
-        <div className="Coworking-container">
-          <p className="text-box"><Link to='/coworkings' style={{ textDecoration: 'none', color: 'white'}}>work</Link></p>
+          <div className="Coworking-container">
+            <p className="text-box"><Link to='/coworkings' style={{ textDecoration: 'none', color: 'white'}}>work</Link></p>
+          </div>
         </div>
-
       </div>
     );
   }
